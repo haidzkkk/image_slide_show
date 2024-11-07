29635015e1bf377b65e0739e8f4ac5deb80a9662
Kantun <bekoi10a6@gmail.com>
2024-11-07 15:21:50 +0700

    clean code plaform

--- android/app/src/main/kotlin/com/example/image_slide_show/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.image_slide_show
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- image_slide_show/lib/image_slide_show.dart
+++
@@ -1,7 +0,0 @@
-library image_slide_show;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
